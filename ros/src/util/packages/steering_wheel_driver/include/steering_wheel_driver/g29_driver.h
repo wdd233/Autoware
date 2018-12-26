716ca8196fe7a51915923a532cb9ede9084437ac
Masaya Kataoka <ms.kataoka@gmail.com>
2018-12-26 16:35:48 +0900

    update CMakeLists.txt

--- ros/src/util/packages/steering_wheel_driver/include/steering_wheel_driver/g29_driver.h
+++ ros/src/util/packages/steering_wheel_driver/include/steering_wheel_driver/g29_driver.h
@@ -8,6 +8,10 @@
 //headers in Autoware
 #include <autoware_msgs/VehicleCmd.h>
 #include <steering_wheel_driver/g29_joyinfo.h>
+#include <std_msgs/UInt16MultiArray.h>
+
+//headers in SDL
+#include <SDL2/SDL.h>
 
 class G29Driver
 {
@@ -20,6 +24,12 @@ private:
     ros::NodeHandle pnh_;
     ros::Publisher cmd_pub_;
     void publishCmd();
+    /*
+    int setup(void);
+    void ActuaterFeedback(const std_msgs::UInt16MultiArray& ActuatorStatus);
+    */
+    int ffUpdate(SDL_Joystick * joystick , unsigned short center);
+    void steerFeedback(const std_msgs::UInt16MultiArray& FFStatus);
 };
 
 #endif  //G29_DRIVER_H_INCLUDED
--- ros/src/util/packages/steering_wheel_driver/src/g29_driver.cpp
+++ ros/src/util/packages/steering_wheel_driver/src/g29_driver.cpp
@@ -20,3 +20,29 @@ void G29Driver::publishCmd()
 {
     return;
 }
+
+/*
+int G29Driver::setup(void)
+{
+
+}
+
+void G29Driver::ActuaterFeedback(const std_msgs::UInt16MultiArray& ActuatorStatus)
+{
+    return;
+}
+*/
+
+int G29Driver::ffUpdate(SDL_Joystick * joystick , unsigned short center)
+{
+    SDL_Haptic *haptic;
+    SDL_HapticEffect effect;
+    int effect_id;
+    haptic = SDL_HapticOpenFromJoystick( joystick );
+    if (haptic == NULL) return -1; // Most likely joystick isn't haptic
+}
+
+void steerFeedback(const std_msgs::UInt16MultiArray& FFStatus)
+{
+    return;
+}
